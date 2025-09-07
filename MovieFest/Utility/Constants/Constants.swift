15c2e52e312dee11a406cdffd91bf062cb32f138
shivajinpawar <shivajinpawar@gmail.com>
2025-09-07 22:37:02 +0530

    Add configuration based variable in info plist

--- MovieFest/Utility/Constants/Constants.swift
+++ MovieFest/Utility/Constants/Constants.swift
@@ -9,7 +9,15 @@ import Foundation
 
 enum API {
     static let baseURL = "https://api.themoviedb.org/3"
-    static let apiKey = "REPLACE_ME"
+    static var apiKey : String{
+        get {
+            guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else {
+                print("Key Not Found")
+                return ""
+            }
+            return apiKey
+        }
+    }
 }
 
 enum MovieServiceType {
